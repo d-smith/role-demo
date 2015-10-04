6b0319732874c1463b8df40664d685aea559cc97
d-smith <doug.smith.mail@gmail.com>
2015-10-04 03:53:48 -0600

    Updated end to end flow to create and display app details

--- handlers/createapp.go
+++ handlers/createapp.go
@@ -1,18 +1,15 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"github.com/nu7hatch/gouuid"
+	"github.com/xtraclabs/roll/roll"
+	"log"
 	"net/http"
 )
-
-type appDetails struct {
-	ClientID         string
-	ApplicationName  string
-	ClientSecret     string
-	RedirectURI      string
-	LoginProvider    string
-	JWTFlowPublicKey string
-}
 
 type devContext struct {
 	Email string
@@ -28,15 +25,72 @@ func handleCreateGet(w http.ResponseWriter, r *http.Request) {
 
 func handleCreatePost(w http.ResponseWriter, r *http.Request) {
 
-	details := appDetails{
-		ApplicationName: "foo app",
-		ClientID:        "id",
-		ClientSecret:    "don't tell",
-		RedirectURI:     "redirect",
-		LoginProvider:   "login provider",
+	log.Println("extract form values and form application detail")
+	u, err := uuid.NewV4()
+	if err != nil {
+		respondError(w, http.StatusInternalServerError, err)
+		return
 	}
 
-	if err := templates.ExecuteTemplate(w, "appdetails.html", details); err != nil {
+	app := roll.Application{
+		DeveloperEmail:  r.FormValue("email"),
+		ClientID:        u.String(),
+		ApplicationName: r.FormValue("appname"),
+		RedirectURI:     r.FormValue("redirecturi"),
+		LoginProvider:   r.FormValue("loginprovider"),
+	}
+
+	log.Println("encode application json")
+	bodyReader := new(bytes.Buffer)
+	enc := json.NewEncoder(bodyReader)
+	err = enc.Encode(app)
+
+	if err != nil {
+		respondError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	log.Println("form put request")
+	requestUrl := fmt.Sprintf("%s/v1/applications/%s", rollEndpoint, app.ClientID)
+	log.Println("put to", requestUrl)
+	req, err := http.NewRequest("PUT", requestUrl, bodyReader)
+	if err != nil {
+		respondError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	log.Println("submit put request")
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("error on put", err.Error())
+		respondError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if resp.StatusCode != http.StatusNoContent {
+		renderErrorPage(w, resp.StatusCode, responseAsString(resp))
+		return
+	}
+
+	log.Println("retrieve full definition to include client secret")
+	retResp, err := http.Get(fmt.Sprintf("%s/v1/applications/%s", rollEndpoint, app.ClientID))
+	if retResp.StatusCode != http.StatusOK {
+		renderErrorPage(w, retResp.StatusCode, responseAsString(resp))
+		return
+	}
+
+	log.Println("decode response into json")
+	var decodedApp roll.Application
+	defer retResp.Body.Close()
+
+	dec := json.NewDecoder(retResp.Body)
+	if err := dec.Decode(&decodedApp); err != nil {
+		respondError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	if err := templates.ExecuteTemplate(w, "appdetails.html", decodedApp); err != nil {
 		respondError(w, http.StatusInternalServerError, err)
 	}
 }
